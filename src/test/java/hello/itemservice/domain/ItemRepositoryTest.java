1923a7a9dfbaa1a6fe89779c5b6a21bd153630de
Oh <osm723@gmail.com>
2024-08-29 15:51:37 +0900

    스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 섹션 9. 다음으로 다음으로

---
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -0,0 +1,71 @@
+package hello.itemservice.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item itemA = new Item("itemA", 10000, 10);
+
+        //when
+        Item savedItem = itemRepository.save(itemA);
+
+        //then
+        Item findItem = itemRepository.findById(savedItem.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Item itemA = new Item("item1", 10000, 10);
+        Item itemB = new Item("item2", 20000, 20);
+        itemRepository.save(itemA);
+        itemRepository.save(itemB);
+
+        //when
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(itemA, itemB);
+    }
+
+    @Test
+    void updateItem() {
+        //given
+        Item item = new Item("item1", 10000, 10);
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        //when
+        Item updateParam = new Item("itme2", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        //then
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+
+    }
+
+
+}
